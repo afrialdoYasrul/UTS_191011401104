f1a3ed5af046dfbff768a19cea8b2fd980d607a6
afrialdoYasrul <103938879+afrialdoYasrul@users.noreply.github.com>
2022-04-18 22:04:15 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,35 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'SYAHRUL RAMADHAN',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Afrialdo Yasrul Nantunggal"),
+          title: Text('Afrialdo Yasrul Nantunggal'),
+          centerTitle: true,
         ),
-        body: Row(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: <Widget>[
-            // ignore: prefer_const_constructors
-            Text("text1"),
-            const Text("text2"),
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 3,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('satu'),
+              ),
+              color: Colors.cyanAccent,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('dua'),
+              ),
+              color: Colors.lightBlueAccent,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('tiga'),
+              ),
+              color: Colors.teal,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('empat'),
+              ),
+              color: Colors.blueGrey,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('lima'),
+              ),
+              color: Colors.blue,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('enam'),
+              ),
+              color: Colors.deepPurple,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('tujuh'),
+              ),
+              color: Colors.lightBlue,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('delapan'),
+              ),
+              color: Colors.greenAccent,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('sembilan'),
+              ),
+              color: Colors.green,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('sepuluh'),
+              ),
+              color: Colors.indigo,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('sebelas'),
+              ),
+              color: Colors.indigoAccent,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: const Text('duabelas'),
+              ),
+              color: Colors.grey,
+            ),
           ],
         ),
       ),
     );
   }
-}
-
-Widget buatKotak(Color warna, double ukuran) {
-  return Container(
-    decoration: BoxDecoration(color: warna),
-    height: ukuran,
-    width: ukuran,
-    margin: const EdgeInsets.all(10),
-  );
 }
